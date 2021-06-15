c4c39669a148d5b2bdcec34d32c2d7328ffc44fe
PidronBatol <pietro.malacalza@greenmail.net>
2021-06-15 22:34:57 +0100

    Added Analysis

--- Analyzing data.py
+++ Analyzing data.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 # import csv file
-filename= 'ESurvey.csv'
+filename = 'ESurvey.csv'
 data = pd.read_csv((filename), low_memory=False)
 
 # Group summary of happiness (mean) by country and gender
@@ -13,6 +13,20 @@ print(happynessbycountry)
 
 # Happiest country (ascending order)
 happiestcountry = data.groupby(['B001'])['C002_01'].mean().sort_values(ascending=False).round(2)
-
 print('Country happiness during Covid time')
 print(happiestcountry)
+
+
+# Function that checks for missing values in age column and replaces with mean
+print('Number of missing values in the AGE column:')
+def check_and_replace():
+    print(data['B003_01'].isnull().sum())
+    data['B003_01'] = data['B003_01'].fillna(data['B003_01'].mean())
+check_and_replace()
+print('Number of missing values in the AGE column after the clean up:')
+print(data['B003_01'].isnull().sum())
+
+#creates firstwave; secondwave; thirdwave filtering by wave field
+firstwave = data.loc[data['wave'] == 1]
+secondwave = data.loc[data['wave'] == 2]
+thirdwave = data.loc[data['wave'] == 3]
