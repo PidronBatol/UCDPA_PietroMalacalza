380eb9edc2ade45f2c639c0a436983e4a05f743a
PidronBatol <pietro.malacalza@greenmail.net>
2021-06-15 21:17:11 +0100

    First round

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+# import packages
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import numpy as np
+
+# import csv file
+filename= 'ESurvey.csv'
+data = pd.read_csv((filename), low_memory=False)
+
+# print heads and some info on dataframe
+print(data.head())
+print(data.info())
+print(data.shape)
+
+# Count number of responses of each country and the proportion
+resp_counts = data['B001'].value_counts()
+resp_prop = data['B001'].value_counts(normalize=True)
+print(resp_counts, resp_prop)
 
